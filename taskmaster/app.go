cb4de6c5e57e53359168f91230179d46ed433054
ozansh <56032671+ozansh@users.noreply.github.com>
2024-12-24 23:30:04 +0300

    created a struct for tasks, wrote the lines to open file with error checking mechanisms

--- taskmaster/app.go
+++ taskmaster/app.go
@@ -2,23 +2,58 @@ package main
 
 import (
 	"fmt"
-	tea "github.com/charmbracelet/bubbletea"
+	"bufio"
+	"io"
+	"os"
+	"log"
 )
 
-
-type model struct {
-    choices  []string           // items on the to-do list
-    cursor   int                // which to-do list item our cursor is pointing at
-    selected map[int]struct{}   // which to-do items are selected
+type Task struct {
+	task, status string
 }
 
+func newTask( task string, status string) *Task {
+	p := Task{task : task}
+	p.status = "done"
+	return &p
+}
+	
 func main(){
+	
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	var file := pwd + "/data/db.json"
+
+	dat, err := os.Readfile(file)
+	if err != nil{
+		if os.IsNotExist(err){
+			f, err2 := os.Create(file)
+			if err2 != nil {
+				log.Fatal(err2)
+			}
+		} else {
+			log.Fatal(err)
+	}
+
 
 
 	fmt.Printf("Initializing database....\n...\n..\n")
-	// db function
+	
 
 	fmt.Printf("Heyoo! Welcome to Taskmaster 2000\n...")
 	fmt.Printf("What would you like to do?\n")
+
+	for {
+
+		fmt.Printf("Add Tasks 		   : Press 1 and enter\n")
+		fmt.Printf("List Tasks 		   : Press 2 and enter\n")
+		fmt.Printf("Mark tasks as done : Press 3 and enter\n")
+		fmt.Printf("Delete tasks	   : Press 4 and enter\n")
+		fmt.Printf("To quit			   : Press q and enter\n")
+
+	}
 
 }
